21247c2010fb0d25bd26675b66493135313e0679
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2017-12-20 12:00:36 -0800

    Apply style classes

--- src/View.elm
+++ src/View.elm
@@ -1,7 +1,7 @@
 module View exposing (view)
 
 import Actions exposing (..)
-import Html exposing (Html, button, div, i, input, text)
+import Html exposing (Html, button, div, h1, i, input, text)
 import Html.Attributes exposing (class, disabled, id, placeholder, style, value)
 import Html.Events exposing (onClick, onInput)
 import Types exposing (..)
@@ -12,15 +12,18 @@ view state =
     div []
         [ div [ class "header" ] []
         , div [ class "container" ]
-            [ div []
-                [ button [ class "button", onClick AddTask ] [ text "add task" ]
-                , button [ class "button", disabled (saveButtonDisabled state), onClick SaveTasks ] [ text "Save" ]
+            [ div [ class "subheader" ]
+                [ h1 [] [ text "Tasks" ]
+                , div [ class "subheader-buttons" ]
+                    [ button [ class "button", onClick AddTask ] [ text "Add task" ]
+                    , button [ class "button", disabled (saveButtonDisabled state), onClick SaveTasks ] [ text "Save" ]
+                    ]
                 ]
             , if state.isLoading && List.length state.tasks == 0 then
-                div [] [ text "Loading" ]
+                i [ class "fa fa-circle-o-notch fa-spin fa-3x fa-fw loader" ] []
               else
                 div [] (List.map taskView state.tasks)
-            , div [] (List.map alertView state.alerts)
+            , div [ class "alerts" ] (List.map alertView state.alerts)
             ]
         ]
 
@@ -34,7 +37,7 @@ taskView : Task -> Html Action
 taskView task =
     div [ class "task" ]
         [ input [ class "task-input", id (toString task.id), placeholder "Add title...", value task.title, onInput (UpdateTask task.id) ] []
-        , i [ class "fa fa-trash", onClick (DeleteTask task) ] []
+        , i [ class "fa fa-trash-o task-delete", onClick (DeleteTask task) ] []
         ]
 
 
@@ -42,14 +45,14 @@ alertView : Alert -> Html Action
 alertView alert =
     let
         render cssClass message =
-            div []
+            div [ class ("alert " ++ cssClass) ]
                 [ text message
-                , i [ class "fa fa-times", onClick (CloseAlert alert) ] []
+                , i [ class "fa fa-times alert-close", onClick (CloseAlert alert) ] []
                 ]
     in
     case alert.content of
         Success message ->
-            render "success" message
+            render "alert-success" message
 
         Error message ->
-            render "error" message
+            render "alert-error" message
